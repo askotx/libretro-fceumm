c9b7d9f9f7d945e7601054b3e4218119e85acb7a
Juan Carlos <askot.altair@gmail.com>
2015-08-11 19:26:47 -0700

    Sort nintendo-vc palette to 4th place

--- src/drivers/libretro/libretro.c
+++ src/drivers/libretro/libretro.c
@@ -29,8 +29,6 @@
 #include <string.h>
 #include "memstream.h"
 
-
-
 static retro_video_refresh_t video_cb = NULL;
 static retro_input_poll_t poll_cb = NULL;
 static retro_input_state_t input_cb = NULL;
@@ -224,6 +222,24 @@ struct st_palettes palettes[] = {
 		   0xffffc0, 0xe0ffc0, 0xc0ffc0, 0xc0ffe0,
 		   0xc0ffff, 0x000000, 0x000000, 0x000000 }
    },
+   { "nintendo-vc", "Virtual Console palette",
+	   { 0x494949, 0x00006a, 0x090063, 0x290059,
+		   0x42004a, 0x490000, 0x420000, 0x291100,
+		   0x182700, 0x003010, 0x003000, 0x002910,
+		   0x012043, 0x000000, 0x000000, 0x000000,
+		   0x747174, 0x003084, 0x3101ac, 0x4b0194,
+		   0x64007b, 0x6b0039, 0x6b2101, 0x5a2f00,
+		   0x424900, 0x185901, 0x105901, 0x015932,
+		   0x01495a, 0x101010, 0x000000, 0x000000,
+		   0xadadad, 0x4a71b6, 0x6458d5, 0x8450e6,
+		   0xa451ad, 0xad4984, 0xb5624a, 0x947132,
+		   0x7b722a, 0x5a8601, 0x388e31, 0x318e5a,
+		   0x398e8d, 0x383838, 0x000000, 0x000000,
+		   0xb6b6b6, 0x8c9db5, 0x8d8eae, 0x9c8ebc,
+		   0xa687bc, 0xad8d9d, 0xae968c, 0x9c8f7c,
+		   0x9c9e72, 0x94a67c, 0x84a77b, 0x7c9d84,
+		   0x73968d, 0xdedede, 0x000000, 0x000000 }
+   },
    { "chris", "Chris Covell's palette",
 	   { 0x808080, 0x003DA6, 0x0012B0, 0x440096,
 		   0xA1005E, 0xC70028, 0xBA0600, 0x8C1700,
@@ -403,24 +419,6 @@ struct st_palettes palettes[] = {
 		   0xf8b8f8, 0xfba7c3, 0xffffff, 0x00ffff,
 		   0xfbdb7b, 0xffa347, 0xb8f8b8, 0xb8f8d8,
 		   0xb8f818, 0xf8d8f8, 0x000000, 0x007800 }
-   },
-   { "nintendo-vc", "Virtual Console palette",
-	   { 0x494949, 0x00006a, 0x090063, 0x290059,
-		   0x42004a, 0x490000, 0x420000, 0x291100,
-		   0x182700, 0x003010, 0x003000, 0x002910,
-		   0x012043, 0x000000, 0x000000, 0x000000,
-		   0x747174, 0x003084, 0x3101ac, 0x4b0194,
-		   0x64007b, 0x6b0039, 0x6b2101, 0x5a2f00,
-		   0x424900, 0x185901, 0x105901, 0x015932,
-		   0x01495a, 0x101010, 0x000000, 0x000000,
-		   0xadadad, 0x4a71b6, 0x6458d5, 0x8450e6,
-		   0xa451ad, 0xad4984, 0xb5624a, 0x947132,
-		   0x7b722a, 0x5a8601, 0x388e31, 0x318e5a,
-		   0x398e8d, 0x383838, 0x000000, 0x000000,
-		   0xb6b6b6, 0x8c9db5, 0x8d8eae, 0x9c8ebc,
-		   0xa687bc, 0xad8d9d, 0xae968c, 0x9c8f7c,
-		   0x9c9e72, 0x94a67c, 0x84a77b, 0x7c9d84,
-		   0x73968d, 0xdedede, 0x000000, 0x000000 }
    }
 };
 
@@ -455,11 +453,10 @@ void retro_set_input_state(retro_input_state_t cb)
 void retro_set_controller_port_device(unsigned a, unsigned b)
 {}
 
-
 void retro_set_environment(retro_environment_t cb)
 {
    static const struct retro_variable vars[] = {
-      { "nes_palette", "Color Palette; asqrealc|loopy|quor|chris|matt|pasofami|crashman|mess|zaphod-cv|zaphod-smb|vs-drmar|vs-cv|vs-smb|nintendo-vc" },
+      { "nes_palette", "Color Palette; asqrealc|loopy|quor|nintendo-vc|chris|matt|pasofami|crashman|mess|zaphod-cv|zaphod-smb|vs-drmar|vs-cv|vs-smb" },
       { NULL, NULL },
    };
 
@@ -566,7 +563,7 @@ static bool fceu_init(const char * full_path)
    return true;
 }
 
-void retro_deinit (void)
+void retro_deinit(void)
 {
    FCEUI_CloseGame();
    FCEUI_Sound(0);
@@ -612,29 +609,29 @@ static void check_variables(void)
          current_palette = 1;
       else if (strcmp(var.value, "quor") == 0)
          current_palette = 2;
-      else if (strcmp(var.value, "chris") == 0)
+      else if (strcmp(var.value, "nintendo-vc") == 0)
          current_palette = 3;
-      else if (strcmp(var.value, "matt") == 0)
+      else if (strcmp(var.value, "chris") == 0)
          current_palette = 4;
       else if (strcmp(var.value, "matt") == 0)
          current_palette = 5;
-      else if (strcmp(var.value, "pasofami") == 0)
+      else if (strcmp(var.value, "matt") == 0)
          current_palette = 6;
-      else if (strcmp(var.value, "crashman") == 0)
+      else if (strcmp(var.value, "pasofami") == 0)
          current_palette = 7;
-      else if (strcmp(var.value, "mess") == 0)
+      else if (strcmp(var.value, "crashman") == 0)
          current_palette = 8;
-      else if (strcmp(var.value, "zaphod-cv") == 0)
+      else if (strcmp(var.value, "mess") == 0)
          current_palette = 9;
-      else if (strcmp(var.value, "zaphod-smb") == 0)
+      else if (strcmp(var.value, "zaphod-cv") == 0)
          current_palette = 10;
-      else if (strcmp(var.value, "vs-drmar") == 0)
+      else if (strcmp(var.value, "zaphod-smb") == 0)
          current_palette = 11;
-      else if (strcmp(var.value, "vs-cv") == 0)
+      else if (strcmp(var.value, "vs-drmar") == 0)
          current_palette = 12;
-      else if (strcmp(var.value, "vs-smb") == 0)
+      else if (strcmp(var.value, "vs-cv") == 0)
          current_palette = 13;
-      else if (strcmp(var.value, "nintendo-vc") == 0)
+      else if (strcmp(var.value, "vs-smb") == 0)
          current_palette = 14;
 
       if (current_palette != orig_value)
